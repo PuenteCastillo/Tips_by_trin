5297a85be42f84f300c8f1ffb85f280e6f18984f
The Chosen one <Josepuente2013@gmail.com>
2018-10-26 12:47:21 -0700

    uploaded files

---
+++ assets/JS/code.js
@@ -0,0 +1,13 @@
+// $(window).scroll(function(){
+//     parallax();
+// })
+// function parallax(){
+//     var wSrcoll = $(window).scrollTop();
+//     $('.mainInfo').css('margin-top', -100  + (-wSrcoll*.3) +'px');
+//     console.log($('.mainInfo').css('margin-top'));
+// }
+
+
+
+
+
